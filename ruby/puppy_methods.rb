d45c6f4679918f5e2ed6e464f6252c9c95f4b31a
nightangelblade <sean.syc.wright@gmail.com>
2016-11-08 12:28:21 -0800

    Edit puppy_methods.rb with Release 0 class methods.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -2,8 +2,41 @@ class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
-    toy
+    puts toy
   end
+    
+    def speak(number)
+        number.times do puts "Woof!"
+        end
+    end
+    
+    def roll_over
+        puts "*rolls over*"
+    end
+    
+    def dog_years(human)
+        p human * 15
+    end
+    
+    def jump(number)
+        jump = 1
+        number.times do puts "Jump!"
+            puts "Your dog has jumped #{jump} times!"
+            jump += 1
+        end
+        
+    end
 
 end
 
+instance = Puppy.new
+
+instance.fetch("stick")
+
+instance.speak(5)
+
+instance.roll_over
+
+instance.dog_years(5)
+
+instance.jump(3)
