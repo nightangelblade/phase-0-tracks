3fee822e847145c9fabb7b34c0315797860cf3fd
nightangelblade <sean.syc.wright@gmail.com>
2016-11-08 19:33:16 -0800

    Add final reflection to bottom of document.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -76,3 +76,21 @@ add_item(food_list, "ice cream", 4)
 remove_item(food_list, "lemonade")
 update_item(food_list, "ice cream", 1)
 pretty_list(food_list)
+
+# What did you learn about pseudocode from working on this challenge?
+# Pseudocoding compared to my initial impressions of it is much simpler than I originally though. It uses for the most part plain English, rather than any terms which are ruby executable, so a layman can look at it and see what the code is going to do.
+
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# While arrays are useful for obtaining intormation, they are not capable of representing values. For the grocery list, we already thought a hash would be more appropriate, as the key: value idea would make more sense. However, at the same time we also had to be sure to understand what methods are capable of being used for both types of data structures.
+
+# What does a method return?
+# A method returns the value of what has been processed through it, so in this case the result/return of each of our methods is the food list we had created.
+
+# What kind of things can you pass into methods as arguments?
+# Pretty much everything we used could be passed as arguments, as they make up the parts of the method, not the whole. Strings, integers, arrays, and hashes were all used as arguments within the methods, and all were applied and executed properly.
+
+# How can you pass information between methods?
+# To pass information between methods, a variable needs to be defined outside of the methods, or else it will be unusable after the method has been processed. In our case, we had to define the food list outside of the methods in order for it to be useable in the ones that follow afterwards.
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# All of the concept we had leanred in week 5 appear to have been solidied with this challenge, and even the optional content about understanding what changes are preserved outside of methods depending on the type put in (data structure vs. variable). I believe for me the part I need to contnue to understand is how methods can be simplfied, and avoiding making things too complex to accomplish a simple task.
