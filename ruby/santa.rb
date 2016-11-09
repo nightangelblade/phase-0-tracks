7c6daad5f487cd751e5441949fdf394f196c838d
nightangelblade <sean.syc.wright@gmail.com>
2016-11-09 15:32:08 -0800

    Edit santa.rb with getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,4 @@
+# Business Code
 class Santa
 
 	def initialize(gender, ethnicity)
@@ -16,8 +17,48 @@ class Santa
 		puts "That was a good #{type}!"
 	end
 
+	def celebrate_birthday
+		@age += 1
+		@age
+	end	
+	
+	# getter methods
+	def age
+		@age
+	end
+	
+	def ethnicity
+		@ethnicity
+	end
+	
+	#setter methods
+	
+	
 end
 
-instance = Santa.new
-instance.speak
-instance.eat_milk_and_cookies("chocolate chip cookie")
+
+# User Interface
+# list = {
+# 	agender: "black",
+# 	female: "Latino",
+# 	bigender: "white",
+# 	male: "Japanese",
+# 	female: "Prefer not to say",
+# 	gender_fluid: "Mystical Creature (unicorn)",
+# 	na: "N/A"
+# }
+
+# list_test = []
+
+# list.each do |gender, race|
+# 	puts "Creating profile"
+# 	list_test << Santa.new(gender, race)
+# end
+
+instance = Santa.new("male", "Asian")
+instance.age
+instance.celebrate_birthday
+instance.age
+# instance = Santa.new
+# instance.speak
+# instance.eat_milk_and_cookies("chocolate chip cookie")
