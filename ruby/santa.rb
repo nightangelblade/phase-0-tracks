a2f7a5196639b4d1065babb95bf27b2f04fb433a
nightangelblade <sean.syc.wright@gmail.com>
2016-11-09 14:51:30 -0800

    Edit santa.rb with added attributes during initialization.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,19 +1,19 @@
 class Santa
 
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
 	def eat_milk_and_cookies(type)
 		puts "That was a good #{type}!"
-	end
-
-	def initialize
-		puts "Initializing Santa instance..."
-		@gender = 
-		@ethnicity = 
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
 	end
 
 end
