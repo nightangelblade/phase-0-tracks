ac6758b66d44c9a4ef396f1a66d7943d6be1429a
nightangelblade <sean.syc.wright@gmail.com>
2016-11-10 20:12:43 -0800

    Edit flight.rb with Release 1 work. Edit shout.rb as mixin module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,40 @@
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		words + "!!!" + " :)"
 	end
 end
 
-require_relative 'shout'
+class Wolf
+	include Shout
+end
 
-p Shout.yell_angrily("Darn")
-p Shout.yelling_happily("Yay")
+class Lion
+	include Shout
+end
+
+wolf = Wolf.new
+wolf.yell_angrily("Aroooo")
+wolf.yelling_happily("Aroooo")
+
+lion = Lion.new
+lion.yell_angrily("Rawr")
+lion.yelling_happily("Rawr")
+
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+# end
+
+# require_relative 'shout'
+
+# p Shout.yell_angrily("Darn")
+# p Shout.yelling_happily("Yay")
