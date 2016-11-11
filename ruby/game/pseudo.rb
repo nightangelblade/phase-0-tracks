a2ddde6478558069ac4cc621319ed781acfa5ad2
nightangelblade <sean.syc.wright@gmail.com>
2016-11-11 14:12:53 -0800

    Create directory and pseudocode file.

---
+++ ruby/game/pseudo.rb
@@ -0,0 +1,13 @@
+=begin
+	
+User 1 enters word or phrase
+Word/Phrase gets stored as the answer
+- Numer of guesses is related to the length of the answer
+- Length of word is displayed as blanks
+User 2 guesses the word or a letter
+- If the word guess matches the answer, game ends
+- If word guess doesn't match the answer, game continues and tell User 2 they are incorrect
+- If letter matches a letter in answer, game continues and display the letter inline with undiscovered letters
+- If letter doesn't match any letters in answer, game continues and tell User 2 they are incorrect
+	
+=end
