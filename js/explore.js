0cf75355e78b2ea9eb6705bdc3fc75cdb4b34c81
nightangelblade <sean.syc.wright@gmail.com>
2016-11-14 23:07:37 -0800

    Create explore.js for 7.1

---
+++ js/explore.js
@@ -0,0 +1,13 @@
+// Take string as parameter
+// - Get length of string
+// - Determine length by zero-index
+// Start and end of string
+// - Start at largest index number
+// - Add individual letter to return value
+// Return value string back
+
+function reverse(string) {
+	index_length = string.length - 1
+
+	for (; index_length)
+}
