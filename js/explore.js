f015e6fa71fdb7481553aef8c3e5b8b0ec7af551
nightangelblade <sean.syc.wright@gmail.com>
2016-11-14 23:27:26 -0800

    Edit with driver code to test if/else statement for pass and fail.

--- js/explore.js
+++ js/explore.js
@@ -1,12 +1,14 @@
-// Take string as parameter
-// - Split string into an array
+// PSEUDOCODE
+// Take string as parameter -
+// - Split string into an array -
 // Method:
-// - Reverse array
-// - Rejoin array back into string
-// Return the joined string
-// Confirm string was returned corretly using boolean
-// - Compare expected result to actual result.
+// - Reverse array -
+// - Rejoin array back into string -
+// Return the joined string -
+// Confirm string was returned corretly using boolean -
+// - Compare expected result to actual result. -
 
+// BUSINESS CODE
 function reverse(string) {
 	var split_string = string.split("");
 	var reverse_array = split_string.reverse();
@@ -14,8 +16,25 @@ function reverse(string) {
 	return reverse_string;
 }
 
+// DRIVER CODE
 console.log(reverse("Hello"));
 
 var r_string = (reverse("Hello"));
+var expected_string = "olleH";
+var expected_string_fail = "olleh";
 
 console.log(r_string == "olleH");
+
+// Test to confirm expected result, expected to pass
+if (r_string == expected_string) {
+	console.log("Actual and expected result match!");
+} else {
+	console.log("Something didn't match! Check the code!");
+}
+
+// Test to confirm unexpected result, expected to fail
+if (r_string == expected_string_fail) {
+	console.log("Actual and expected result match!");
+} else {
+	console.log("Something didn't match! Check the code!");
+}
