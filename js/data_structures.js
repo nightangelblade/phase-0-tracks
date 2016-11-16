c1e902d62c53bc08bc3ae9525bdce1fcd77b2888
nightangelblade <sean.syc.wright@gmail.com>
2016-11-16 14:23:57 -0800

    Uncommented out horse section.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,25 +1,24 @@
-// var colors = ["blue", "red", "rasberry", "ombre"];
-// var names = ["Spirit", "Maximus", "Lasty", "Amber"];
+var colors = ["blue", "red", "rasberry", "ombre"];
+var names = ["Spirit", "Maximus", "Lasty", "Amber"];
 
+console.log(colors);
+console.log(names);
 
-// console.log(colors);
-// console.log(names);
+colors.push("almond brown");
+names.push("Captain");
 
-// colors.push("almond brown");
-// names.push("Captain");
+console.log(colors);
+console.log(names);
 
-// console.log(colors);
-// console.log(names);
+function horses(name, color) {
+	var horses = {};
+	for (var index = 0; index < name.length; index++){
+	horses[name[index]] = color[index];
+		}
+	return horses
+}
 
-// function horses(name, color) {
-// 	var horses = {};
-// 	for (var index = 0; index < name.length; index++){
-// 		horses[name[index]] = color[index];
-// 	}
-// 	return horses
-// }
-
-// console.log(horses(names, colors));
+console.log(horses(names, colors));
 
 function Car(type, drive, isFamilySafe){
 	console.log("Our new car:", this);
