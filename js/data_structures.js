5659240d3457131765e519a63d214661077d3cdd
nightangelblade <sean.syc.wright@gmail.com>
2016-11-16 13:43:20 -0800

    Edit with function to add arrays to object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,22 @@
 var colors = ["blue", "red", "rasberry", "ombre"];
 var names = ["Spirit", "Maximus", "Lasty", "Amber"];
+
+
+// console.log(colors);
+// console.log(names);
+
+colors.push("almond brown");
+names.push("Captain");
+
+// console.log(colors);
+// console.log(names);
+
+function horses(name, color) {
+	var horses = {};
+	for (var index = 0; index < name.length; index++){
+		horses[name[index]] = color[index];
+	}
+	return horses
+}
+
+console.log(horses(names, colors));
