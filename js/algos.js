6ee5402a38186b70fdb0dc5a277612c5e66be580
nightangelblade <sean.syc.wright@gmail.com>
2016-11-16 20:09:30 -0800

    Edit with block methods and test code for various parts of method.

--- js/algos.js
+++ js/algos.js
@@ -6,6 +6,26 @@
 //	- Find phrase with matching length
 //- Returns the longest word/phrase
 
+//var test = ["test", "longest phrase ever", "long phrase"]
+
+function longest_phrase(array){
+	var length = [];
+
+	for (var index = 0; index < array.length; index++){
+			length.push(array[index].length);
+	}
+
+	for (var index = 0; index < array.length; index++){
+		if (array[index].length == Math.max.apply(Math, length)){
+			console.log(array[index]);
+		}
+		else;
+	}
+}
+
+longest_phrase(["test", "longest phrase ever", "long phrase"])
+longest_phrase(["so long it is incomprehensibly long", "kinda long", "super long"])
+longest_phrase(["longitylonglong", "long", "looooooooooooooooooooooooooooooong"])
 
 
 
