59bceb7fb79dc74ee3c187b9df6d37e476e9cbe8
nightangelblade <sean.syc.wright@gmail.com>
2016-11-17 00:07:33 -0800

    Edit with array_longest method for Release 2.

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,7 @@
 // Release 0
+
+// PSEUDOCODE
+
 //- Take array of words and/or phrases
 //- Determine the length of each individual word/phrases
 //	- Store lengths in array
@@ -8,30 +11,38 @@
 
 //var test = ["test", "longest phrase ever", "long phrase"]
 
-// function longest_phrase(array){
-// 	var length = [];
+//BUSINESS CODE
 
-// 	for (var index = 0; index < array.length; index++){
-// 			length.push(array[index].length);
-// 	}
+function longest_phrase(array){
+	var length = [];
 
-// 	for (var index = 0; index < array.length; index++){
-// 		if (array[index].length == Math.max.apply(Math, length)){
-// 			console.log(array[index]);
-// 		}
-// 		else;
-// 	}
-// }
+ 	for (var index = 0; index < array.length; index++){
+ 			length.push(array[index].length);
+ 	}
 
+ 	for (var index = 0; index < array.length; index++){
+ 		if (array[index].length == Math.max.apply(Math, length)){
+ 			console.log(array[index]);
+ 		}
+ 		else;
+ 	}
+}
+
+// DRIVER CODE
 // longest_phrase(["test", "longest phrase ever", "long phrase"])
 // longest_phrase(["so long it is incomprehensibly long", "kinda long", "super long"])
 // longest_phrase(["longitylonglong", "long", "looooooooooooooooooooooooooooooong"])
 
 // Release 1
+
+// PSEUDOCODE
+
 // - Take two objects
 // - Iterate through objects
 // 	- If there is at least one match of key and value, return true
 // 	- If there is no match of key and value, return false
+
+// BUSINESS CODE
 
 // var test = {favorite_number: 1, name: "Test", age: 53};
 // var test_true = {favorite_number: 1, name: "Testy", age: 54};
@@ -56,6 +67,8 @@
 // }
 // };
 
+// DRIVER CODE
+
 // console.log("This is the baseline.");
 // console.log(test);
 
@@ -68,12 +81,17 @@
 // console.log(comparison_test(test, test_false));
 
 // Release 2
+
+// PSEUDOCODE
+
 // - Takes integer
 // - Builds array of length of integer (add to blank array so many times)
 // - Fills array with random words
 //	- Words need to be of random lengths between 1-10 letters.
 //		- Create words by filling a word array with random letters from alphabet.
 //		- Join word array to create word, then add to main array.
+
+// BUSINESS CODE
 
 function array_generator(amount) {
 	var array = [];
@@ -91,4 +109,16 @@ function array_generator(amount) {
 	return array;
 };
 
-console.log(array_generator(3));
+function array_longest(amount) {
+	for (var count = 0; count < amount; count ++){
+		generated_array = array_generator(Math.floor((Math.random() * 5)+1));
+		console.log(generated_array);
+		longest_phrase(generated_array);
+	}
+}
+
+// DRIVER CODE
+
+array_longest(10);
+
+
