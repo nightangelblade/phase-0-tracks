35b57cfd3db7539f0df3558e93ec5a150d962206
nightangelblade <sean.syc.wright@gmail.com>
2016-11-16 20:07:10 -0800

    Create algos.js and add pseudocode.

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// Release 0
+//- Take array of words and/or phrases
+//- Determine the length of each individual word/phrases
+//	- Store lengths in array
+//	- Determine largest number in the array
+//	- Find phrase with matching length
+//- Returns the longest word/phrase
+
+
+
+
