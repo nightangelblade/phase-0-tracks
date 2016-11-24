a90ccc694b976777bfc92aa30b66c3f4334cae0c
nightangelblade <sean.syc.wright@gmail.com>
2016-11-24 01:02:38 -0800

    Edit with code for creating two tables. Currently looking at issue with sqlite3 not loading when executing ruby file.

--- databases/awesome_project/awesome_project.rb
+++ databases/awesome_project/awesome_project.rb
@@ -2,11 +2,14 @@
 PSEUDOCODE
 The Create, Store, and Update a Character Machine!
 - require respective gems
-	- SQLite3 for database
+	- SQLite3 for database-
 	- Possibly Faker if numerous names need to be generated.
+- Create database-
+	- Create table for storing characters- 
+	- Create table for storing campaigns- 
 - User Interface
 	- Upon loading, ask if the user is creating a new character, updating a current one, or deleting an old one.
-		- If creating a new character, create a new table
+		- If creating a new character, create a new row
 			- Walk user through each part of the table until entire table is filled with information.
 		- If updating a current one, ask the user for the name of the character they had created (make note for correct syntax). Additionaly give user ability to see all stored characters if they don't remember.
 			- After selecting character, ask user what (row) they are updating
@@ -19,6 +22,48 @@ The Create, Store, and Update a Character Machine!
 =end
 
 # BUSINESS CODE
+require 'sqlite3'
+
+character_database = SQLite3::Database.new("characters.db")
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS characters(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    class VARCHAR(255),
+    level INT,
+    personality VARCHAR(255),
+    strengths VARCHAR(255),
+    weaknesses VARCHAR(255),
+    background VARCHAR(255)
+    campaign_id INT,
+    FOREIGN KEY (campaign_id) REFERENCES campaigns(id)
+  )
+SQL
+
+create_campaign_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS campaigns(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    system VARCHAR(255),
+    theme VARCHAR(255),
+    level VARCHAR(255)
+  )
+SQL
+
+
+def new_character()
+	#character_database.execute("INSERT INTO characters (name, age, class, level, personality, strengths, weaknesses, background, campaign_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [name, age, class, level, personality, strengths, weaknesses, background, campaign_idname, age, class, level, personality, strengths, weaknesses, background, campaign_id])
+end
+
+def update_character()
+
+end
+
+def delete_character()
+
+end
 
 
 # DRIVER CODE
