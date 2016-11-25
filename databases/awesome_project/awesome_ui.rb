abcad3148802b0cdd90e31a5bafaae135886825c
nightangelblade <sean.syc.wright@gmail.com>
2016-11-24 23:47:14 -0800

    Edit with interface for updating (testing) and deleting (tested) characters.

--- databases/awesome_project/awesome_ui.rb
+++ databases/awesome_project/awesome_ui.rb
@@ -48,10 +48,39 @@ if action == "new"
 elsif action == "update"
 
 	puts "Which character are you updating?"
-	character_name = gets.chomp
-
+	update_character_name = gets.chomp
+	puts "What part of the profile are you editing? (name, age, class, level, campaign)"
+	edit = gets.chomp
+	if edit == "name"
+		puts "What is the new name for your character?"
+		new_name = gets.chomp
+		update_character_name(character_database, new_name, update_character_name)
+	elsif edit == "age"
+		puts "What is the new age for your character?"
+		new_age = gets.chomp.to_i
+		update_character_age(character_database, new_age, update_character_name)
+	elsif edit == "class"
+		puts "What is the new class for your character?"
+		new_class = gets.chomp
+		update_character_class(character_database, new_class, update_character_name)
+	elsif edit == "level"
+		puts "What is the new class for your character?"
+		new_level = gets.chomp.to_i
+		update_character_level(character_database, new_class, update_character_name)
+	elsif edit == "campaign"
+		puts "What is the new campaign your character is playing in?"
+		p character_database.execute("SELECT * FROM campaigns")
+		new_campaign = gets.chomp.to_i
+		update_character_campaign(character_database, new_campaign, update_character_name)
+	else
+		puts "Sorry I missed that. Are you updating something for your character?"
+	end
 
 elsif action == "delete"
+
+	puts "Which character are you deleting?"
+	delete_character_name = gets.chomp
+	delete_character(character_database, delete_character_name)
 
 else
 	puts "Sorry, I didn't catch that. What were you trying to do? (Type 'new', 'update', or 'delete')"
