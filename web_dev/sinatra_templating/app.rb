581926170cefd34abf00d21f676df30982157ab2
nightangelblade <sean.syc.wright@gmail.com>
2016-12-02 14:26:59 -0800

    Edit with work for 9.5

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -10,6 +10,7 @@ db.results_as_hash = true
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  @campus = db.execute("SELECT * FROM campuses")
   erb :home
 end
 
@@ -23,5 +24,27 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+get '/by_campus' do
+  @students = db.execute("SELECT * FROM students ORDER BY campus")
+  erb :template
+end
+
+get '/campuses/new' do
+  erb :newcampus
+end
+
+post '/campuses' do
+  db.execute("INSERT INTO campuses (campus) VALUES (?)", [params['campus']])
+  redirect '/'
+end
+
+# get '/campuses/list' do
+#   @campus = db.execute("SELECT * FROM campuses")
+#   erb :campuslist
+# end
+
+
+
 
 # add static resources
